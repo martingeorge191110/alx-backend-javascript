d60f9dfc99730074ecbde639fa683b2b38976757
Martin George <martingeorge191110@gmail.com>
2025-04-14 18:28:56 +0200

    Task 0 and 1

--- 0x00-ES6_basic/babel.config.js
+++ 0x00-ES6_basic/babel.config.js
@@ -1,35 +1,12 @@
 module.exports = {
-   env: {
-      browser: false,
-      es6: true,
-      jest: true,
-   },
-   extends: [
-      'airbnb-base',
-      'plugin:jest/all',
-   ],
-   globals: {
-      Atomics: 'readonly',
-      SharedArrayBuffer: 'readonly',
-   },
-   parserOptions: {
-      ecmaVersion: 2018,
-      sourceType: 'module',
-   },
-   plugins: ['jest'],
-   rules: {
-      'no-console': 'off',
-      'no-shadow': 'off',
-      'no-restricted-syntax': [
-         'error',
-         'LabeledStatement',
-         'WithStatement',
+   presets: [
+      [
+         '@babel/preset-env',
+         {
+            targets: {
+               node: 'current',
+            },
+         },
       ],
-   },
-   overrides: [
-      {
-         files: ['*.js'],
-         excludedFiles: 'babel.config.js',
-      }
-   ]
+   ],
 };
