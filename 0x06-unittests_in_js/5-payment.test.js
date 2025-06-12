aceb07e2980014936864df70434c2713ca0de0e2
Martin George <martingeorge191110@gmail.com>
2025-06-12 16:24:39 +0300

    Task 5

---
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -0,0 +1,28 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+const sendPaymentRequestToApi = require('./5-payment');
+const Utils = require('./utils');
+
+describe('sendPaymentRequestToApi', function () {
+  let consoleSpy;
+
+  beforeEach(() => {
+    consoleSpy = sinon.spy(console, 'log');
+  });
+
+  afterEach(() => {
+    consoleSpy.restore();
+  });
+
+  it('sendPaymentRequestToApi prints The total is: 120', function () {
+    sendPaymentRequestToApi(100, 20);
+    expect(consoleSpy.calledWith('The total is: 120')).to.be.true;
+    expect(consoleSpy.calledOnce).to.be.true;
+  });
+
+  it('sendPaymentRequestToApi prints The total is: 20', function () {
+    sendPaymentRequestToApi(10, 10);
+    expect(consoleSpy.calledWith('The total is: 20')).to.be.true;
+    expect(consoleSpy.calledOnce).to.be.true;
+  });
+});
